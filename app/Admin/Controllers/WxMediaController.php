ce6373ecc6075f7e3257da9ca05d3d0a11011bad
guodongchao <1015439000@qq.com>
2019-02-21 10:19:45 +0800

    素材

--- app/Admin/Controllers/WxMediaController.php
+++ app/Admin/Controllers/WxMediaController.php
@@ -88,7 +88,9 @@ class WxMediaController extends Controller
         $grid->media_id('Media id');
         $grid->format('Format');
         $grid->msg_id('Msg id');
-        $grid->local_file_name('Local file name');
+        $grid->local_file_name('Local file name')->display(function($data) {
+        return '<img src="/wx/images/' . $data .' " width=50px;height=50px;>';
+    });;
         $grid->local_file_path('Local file path');
 
         return $grid;

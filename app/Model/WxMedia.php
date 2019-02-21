29cfb4404dd277a96aa4b02c7a695ab319aca4eb
guodongchao <1015439000@qq.com>
2019-02-21 09:39:57 +0800

    素材

---
+++ app/Model/WxMedia.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WxMedia extends Model
+{
+    //
+    public $table = 'p_wx_media';
+    public $timestamps = false;
+}

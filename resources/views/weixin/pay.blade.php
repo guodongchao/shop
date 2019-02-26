b3b0d3e04d66c38624df237aa44c98aae654f40a
guodongchao <1015439000@qq.com>
2019-02-26 16:34:52 +0800

    a

--- resources/views/weixin/pay.blade.php
+++ resources/views/weixin/pay.blade.php
@@ -7,3 +7,21 @@
     <script src="{{ asset('/qrcodejs/qrcode.js')}}"></script>
 @endsection
 
+<script>
+    // 简单方式
+    new QRCode(document.getElementById('qrcode'), 'your content');
+
+    // 设置参数方式
+    var qrcode = new QRCode('qrcode', {
+        text: 'your content',
+        width: 256,
+        height: 256,
+        colorDark : '#000000',
+        colorLight : '#ffffff',
+        correctLevel : QRCode.CorrectLevel.H
+    });
+
+    // 使用 API
+    qrcode.clear();
+    qrcode.makeCode('new content');
+</script>

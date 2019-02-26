689db7172cbff50d4ec020e7d6f09135365c640b
guodongchao <1015439000@qq.com>
2019-02-26 14:09:19 +0800

    支付

--- routes/web.php
+++ routes/web.php
@@ -133,5 +133,7 @@ Route::get('/weixin/chat/get_msgs','Weixin\WeixinController@getChatMsgs');     /
 
 //微信支付
 Route::get('/weixin/pay/test','Weixin\PayController@test');     //微信支付测试
-Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调a
+Route::get('/weixin/pay/test','Weixin\PayController@test');     //微信支付测试
+Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调
+Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调
 

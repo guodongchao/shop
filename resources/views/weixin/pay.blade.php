69a29f43a1f511a7f158dad92b9518abaf063b4f
guodongchao <1015439000@qq.com>
2019-02-26 17:02:06 +0800

    a

--- resources/views/weixin/pay.blade.php
+++ resources/views/weixin/pay.blade.php
@@ -1,27 +1,52 @@
-@extends('layout.main')
-@section('content')
-    <div id="qrcode"></div>
-@endsection
-@section('footer')
-    @parent
-    <script src="{{ asset('/qrcodejs/qrcode.js')}}"></script>
-@endsection
+
+<script src="{{ asset('/qrcodejs/qrcode.js')}}"></script>
+
+<div id="qrcode" style="width: 560px;height: 560px;background-color: white;"></div>
+</div>
+
+
+</div>
+</template>
 
 <script>
-    // 简单方式
-    new QRCode(document.getElementById('qrcode'), 'your content');
-
-    // 设置参数方式
-    var qrcode = new QRCode('qrcode', {
-        text: 'your content',
-        width: 256,
-        height: 256,
-        colorDark : '#000000',
-        colorLight : '#ffffff',
-        correctLevel : QRCode.CorrectLevel.H
-    });
-
-    // 使用 API
-    qrcode.clear();
-    qrcode.makeCode('new content');
+    import QRCode from 'qrcodejs2';
+    import Cookie from 'vue-cookies';
+    export default {
+        name: "binding-weixin",
+        data() {
+            return {
+                qrcode: ''
+            }
+        },
+        methods: {
+            getBindUserQcode: function () {
+                let csrftoken = Cookie.get('csrftoken');
+                this.$axios.request({
+                    url: this.$store.state.apiList.bindingWeixin,
+                    method: 'get',
+                    // params: {foo: 'bar'},
+                    headers: {"X-CSRFToken": csrftoken}
+                }).then(res => {
+                    this.qrcode.clear();
+                    this.qrcode.makeCode(res.data.data);
+                    console.log(res.data.data, 22222222222222222)
+                }).catch(res => {
+                    console.log(res, 1111111111111111111)
+                })
+            }
+        },
+        mounted() {
+            this.qrcode = new QRCode('qrcode', {
+                text: '',
+                width: 200,
+                height: 200,
+                // render: 'canvas' // 设置渲染方式（有两种方式 table和canvas，默认是canvas）
+                colorDark: '#000000',
+                colorLight: '#ffffff',
+                correctLevel: 3
+            })
+        }
+    }
 </script>
+
+

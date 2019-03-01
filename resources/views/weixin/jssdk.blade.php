f99a4ea55206f3d1bc9471f1cc4a23416d8dc081
guodongchao <1015439000@qq.com>
2019-03-01 13:48:54 +0800

    saa

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -8,7 +8,6 @@
 @endsection
 @section('footer')
     @parent
-    <script src="{{URL::asset('/js/weixin/chat.js')}}"></script>
     <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 
     <script>

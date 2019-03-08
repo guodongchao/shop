350e999247dbdc34e20a95a4669be44dd1ad4645
guodongchao <1015439000@qq.com>
2019-03-08 09:37:03 +0800

    a

--- app/Admin/Controllers/ShowController.php
+++ app/Admin/Controllers/ShowController.php
@@ -87,7 +87,9 @@ class ShowController extends Controller
         $grid->add_time('Add time');
         $grid->nickname('Nickname');
         $grid->sex('Sex');
-        $grid->headimgurl('Headimgurl');
+        $grid->headimgurl('Headimgurl')->disp(function($data){
+            return "<img src=".$data.">";
+        });
         $grid->subscribe_time('Subscribe time');
 
         return $grid;

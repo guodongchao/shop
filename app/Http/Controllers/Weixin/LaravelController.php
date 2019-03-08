861c16e998e1c50307fea5d16dd5331654dae059
guodongchao <1015439000@qq.com>
2019-03-08 09:20:59 +0800

    dsa

--- app/Http/Controllers/Weixin/LaravelController.php
+++ app/Http/Controllers/Weixin/LaravelController.php
@@ -15,7 +15,8 @@ class LaravelController extends Controller
 {
     protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
     protected $redis_weixin_jsapi_ticket = 'str:weixin_jsapi_ticket';     //微信 access_token
-    
+
+        /**接受微信推送事件*/
     public function wxEvent()
     {
         $data = file_get_contents("php://input");
@@ -66,5 +67,37 @@ class LaravelController extends Controller
 
         }
 
+    /**
+     * 获取微信AccessToken
+     */
+    public function getWXAccessToken()
+    {
+        //获取缓存
+        $token = Redis::get($this->redis_weixin_access_token);
+        if(!$token){        // 无缓存 请求微信接口
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WEIXIN_APPID').'&secret='.env('WEIXIN_APPSECRET');
+            $data = json_decode(file_get_contents($url),true);
 
+            //记录缓存
+            $token = $data['access_token'];
+            Redis::set($this->redis_weixin_access_token,$token);
+            Redis::setTimeout($this->redis_weixin_access_token,3600);
+        }
+        return $token;
+
+    }
+
+    /**
+     * 获取用户信息
+     * @param $openid
+     */
+    public function getUserInfo($openid)
+    {
+        //$openid = 'oLreB1jAnJFzV_8AGWUZlfuaoQto';
+        $access_token = $this->getWXAccessToken();
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $data = json_decode(file_get_contents($url),true);
+        //echo '<pre>';print_r($data);echo '</pre>';
+        return $data;
+    }
 }

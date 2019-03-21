58702abc536a30c63d9a69df303b0257d8d04d0d
guodongchao <1015439000@qq.com>
2019-03-21 16:37:07 +0800

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('http://api3.com/userlogin') }}">Login</a>
-                        <a href="{{ route('http://passport.cms.com/login') }}">Register</a>
+                        <a href="http://api3.com/userlogin">Login</a>
+                        <a href="http://passport.cms.com/login">Register</a>
                     @endauth
                 </div>
             @endif
